fa9659520153915f308a51116cf357f370f2d305
Allen Galler <allengaller@gmail.com>
2014-02-23 20:40:30 +0800

    update
    update

--- web/public_html/app/Config/routes.php
+++ web/public_html/app/Config/routes.php
@@ -24,11 +24,14 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+	//Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+    Router::connect('/', array('controller'=>'main', 'action'=>'index'));
+    Router::connect('/index', array('controller'=>'main', 'action'=>'index'));
+    Router::connect('/logout', array('controller'=>'main', 'action'=>'logout'));
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
-	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+	//Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 
 /**
  * Load all plugin routes. See the CakePlugin documentation on
--- web/public_html/app/View/Main/home.ctp
+++ web/public_html/app/View/Main/home.ctp
@@ -1,1 +1,10 @@
-home for user
+<div class="slide_banner">
+    <dl class="login_home">
+        <dd>
+            <form class="tform" id="formlogin" name="formlogin" method="post" action="<?php echo Router::url(array('controller'=>'Main', 'action' => 'login'))?>">
+                <p>User Name <input type="text" name="name" value="" class="input_login" placeholder="" ></p>
+                <p>Password<input type="password" name="password" class="input_login" placeholder="" ></p>
+            </form>
+        </dd>
+    </dl>
+</div>
